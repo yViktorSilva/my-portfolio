53ba1930c07553d174ebfbe2b3c5662648042ebe
Viktor Lima <v.silvalima.1996@gmail.com>
2025-02-12 00:50:57 -0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Configuração específica para produção
+  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/', // URL base para produção
   plugins: [react()],
-  base: '/my-portfolio/', // Nome do repositório no GitHub
-})
+  define: {
+    // Variáveis de ambiente para produção, se necessário
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  build: {
+    // Configurações de build para produção
+    sourcemap: false, // Desativa o sourcemap em produção (opcional)
+    minify: 'esbuild', // Minificação do código em produção
+    assetsInlineLimit: 4096, // Limita o tamanho dos assets in-line
+  },
+});
